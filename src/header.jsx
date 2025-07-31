063cf5a89b4221fa56113cc703e7eb784bcd1ca6
Srimathi1306 <srimathi600201@gmail.com>
2025-07-31 19:34:27 +0530

    Configure Tailwind CSS with Vite setup

--- src/header.jsx
+++ src/header.jsx
@@ -1,3 +1,10 @@
 export default function header() {
-  return <h1>COUNTER-APP</h1>;
+  return (
+    <section>
+      <header className="bg-blue-50 p-8  flex justify-center items-center">
+        {" "}
+        <h1 className="font-bold text-4xl">COUNTER APP</h1>
+      </header>
+    </section>
+  );
 }
