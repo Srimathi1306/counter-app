fd166d86d295a805aa861b1f79de816f2ef34470
Srimathi1306 <srimathi600201@gmail.com>
2025-08-04 23:52:23 +0530

    Fix onClick event handlers and correct state update logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-export default function Counter() {
-  return <h1>app</h1>;
+import { useState } from "react";
+export default function App(props) {
+  const [count, setcount] = useState(0);
+  function increment() {
+    setcount((pre) => pre + 1);
+  }
+  function decrement() {
+    setcount((pre) => pre - 1);
+  }
+  return (
+    <div className="flex justify-center items-center gap-4">
+      <div className="p-8 bg-blue-100 h-25 w-25 m-10 rounded-full flex justify-center items-center">
+        <h1 className="font-bold text-3xl"> {count}</h1>
+      </div>
+      <button onClick={increment} className="font-bold text-4xl">
+        +
+      </button>
+      <button onClick={decrement} className="font-bold text-4xl">
+        -
+      </button>
+    </div>
+  );
 }
