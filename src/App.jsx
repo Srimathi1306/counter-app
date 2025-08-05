20120162073c6c2c7b19580306d583c05600a536
Srimathi1306 <srimathi600201@gmail.com>
2025-08-05 22:00:47 +0530

    added styling to counter app

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,20 @@ export default function App(props) {
     setcount((pre) => pre - 1);
   }
   return (
-    <div className="flex justify-center items-center gap-4">
-      <div className="p-8 bg-blue-100 h-25 w-25 m-10 rounded-full flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-4">
+      <div className="p-8 bg-blue-100 h-25 w-25 m-10 rounded-full flex flex-row justify-center items-center">
         <h1 className="font-bold text-3xl"> {count}</h1>
       </div>
-      <button onClick={increment} className="font-bold text-4xl">
+      <button
+        onClick={increment}
+        className="font-bold text-4xl w-16 h-16 bg-green-400 hover:bg-green-300 text-white rounded-full shadow-md transition"
+      >
         +
       </button>
-      <button onClick={decrement} className="font-bold text-4xl">
+      <button
+        onClick={decrement}
+        className="font-bold text-4xl w-16 h-16 bg-red-400 hover:bg-red-300 text-white rounded-full shadow-md transition"
+      >
         -
       </button>
     </div>
