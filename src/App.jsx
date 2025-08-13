50d142c1a31260c462ddfbb231dcf7cc584a3b10
Srimathi1306 <srimathi600201@gmail.com>
2025-08-13 18:46:41 +0530

    updated tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,37 @@ export default function App(props) {
     setcount((pre) => pre - 1);
   }
   return (
-    <div className="flex flex-col justify-center items-center gap-4">
+    <div className="flex flex-col justify-center items-center gap-4 m-8 ">
       <div className="p-8 bg-blue-100 h-25 w-25 m-10 rounded-full flex flex-row justify-center items-center">
         <h1 className="font-bold text-3xl transition-transform duration-200 ease-in-out transform scale-105">
           {" "}
           {count}
         </h1>
       </div>
-      <div className="flex gap-4">
+      <div className=" border-2 border-gray-400 m-8 p-8">
+        <div className="flex gap-4">
+          <button
+            onClick={increment}
+            title="Increase"
+            className=" rounded hover:border hover:border-blue-500 font-bold text-4xl w-16 h-16 bg-green-400 hover:bg-green-300 text-white  delay-300 rounded-full shadow-md transition"
+          >
+            +
+          </button>
+          <button
+            onClick={decrement}
+            title="decrease"
+            className="rounded hover:border hover:border-blue-500 font-bold text-4xl w-16 h-16 bg-red-400 hover:bg-red-300 text-white delay-300 rounded-full shadow-md transition"
+          >
+            -
+          </button>
+        </div>
         <button
-          onClick={increment}
-          title="Increase"
-          className=" rounded hover:border hover:border-blue-500 font-bold text-4xl w-16 h-16 bg-green-400 hover:bg-green-300 text-white  delay-300 rounded-full shadow-md transition"
+          onClick={() => setcount(0)}
+          className=" m-4 rounded font-bold text-xl px-6 py-2 bg-gray-500 hover:bg-gray-400 text-white delay-200 shadow-md transition"
         >
-          +
-        </button>
-        <button
-          onClick={decrement}
-          title="decrease"
-          className="rounded hover:border hover:border-blue-500 font-bold text-4xl w-16 h-16 bg-red-400 hover:bg-red-300 text-white delay-300 rounded-full shadow-md transition"
-        >
-          -
+          Reset
         </button>
       </div>
-      <button
-        onClick={() => setcount(0)}
-        className="rounded font-bold text-xl px-6 py-2 bg-gray-500 hover:bg-gray-400 text-white delay-300 shadow-md transition"
-      >
-        Reset
-      </button>
     </div>
   );
 }
